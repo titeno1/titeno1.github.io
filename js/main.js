a62a631697427362aa1edca367e34c29ad5ee380
titeno1 <titeno1231@gmail.com>
2018-11-06 19:10:13 +0700

    update 1.3

--- js/main.js
+++ js/main.js
@@ -5,7 +5,187 @@ $(document).ready(function () {
             scrollTop: $(this.hash).offset().top
         }, 900);
     });
-   
+    /*
+    1. Vars and Inits
+*/
+
+    var header = $('.header');
+    var menuActive = false;
+    var menu = $('.menu');
+    var burger = $('.hamburger');
+    var ctrl = new ScrollMagic.Controller();
+
+    setHeader();
+
+    $(window).on('resize', function () {
+        setHeader();
+    });
+
+    $(document).on('scroll', function () {
+        setHeader();
+    });
+
+    initMenu();
+    initHeaderSearch();
+    initHomeSlider();
+    initMilestones();
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+    function setHeader() {
+        if ($(window).scrollTop() > 100) {
+            header.addClass('scrolled');
+        }
+        else {
+            header.removeClass('scrolled');
+        }
+    }
+
+	/* 
+
+	3. Init Menu
+
+	*/
+
+    function initMenu() {
+        if ($('.menu').length) {
+            var menu = $('.menu');
+            if ($('.hamburger').length) {
+                burger.on('click', function () {
+                    if (menuActive) {
+                        closeMenu();
+                    }
+                    else {
+                        openMenu();
+
+                        $(document).one('click', function cls(e) {
+                            if ($(e.target).hasClass('menu_mm')) {
+                                $(document).one('click', cls);
+                            }
+                            else {
+                                closeMenu();
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    }
+
+    function openMenu() {
+        menu.addClass('active');
+        menuActive = true;
+    }
+
+    function closeMenu() {
+        menu.removeClass('active');
+        menuActive = false;
+    }
+
+	/* 
+
+	4. Init Header Search
+
+	*/
+
+    function initHeaderSearch() {
+        if ($('.search_button').length) {
+            $('.search_button').on('click', function () {
+                if ($('.header_search_container').length) {
+                    $('.header_search_container').toggleClass('active');
+                }
+            });
+        }
+    }
+
+	/* 
+
+	5. Init Home Slider
+
+	*/
+
+    function initHomeSlider() {
+        if ($('.home_slider').length) {
+            var homeSlider = $('.home_slider');
+            homeSlider.owlCarousel(
+                {
+                    items: 1,
+                    loop: true,
+                    autoplay: true,
+                    nav: false,
+                    dots: false,
+                    smartSpeed: 1200
+                });
+
+            if ($('.home_slider_prev').length) {
+                var prev = $('.home_slider_prev');
+                prev.on('click', function () {
+                    homeSlider.trigger('prev.owl.carousel');
+                });
+            }
+
+            if ($('.home_slider_next').length) {
+                var next = $('.home_slider_next');
+                next.on('click', function () {
+                    homeSlider.trigger('next.owl.carousel');
+                });
+            }
+        }
+    }
+
+	/* 
+
+	6. Initialize Milestones
+
+	*/
+
+    function initMilestones() {
+        if ($('.milestone_counter').length) {
+            var milestoneItems = $('.milestone_counter');
+
+            milestoneItems.each(function (i) {
+                var ele = $(this);
+                var endValue = ele.data('end-value');
+                var eleValue = ele.text();
+
+	    		/* Use data-sign-before and data-sign-after to add signs
+	    		infront or behind the counter number */
+                var signBefore = "";
+                var signAfter = "";
+
+                if (ele.attr('data-sign-before')) {
+                    signBefore = ele.attr('data-sign-before');
+                }
+
+                if (ele.attr('data-sign-after')) {
+                    signAfter = ele.attr('data-sign-after');
+                }
+
+                var milestoneScene = new ScrollMagic.Scene({
+                    triggerElement: this,
+                    triggerHook: 'onEnter',
+                    reverse: false
+                })
+                    .on('start', function () {
+                        var counter = { value: eleValue };
+                        var counterTween = TweenMax.to(counter, 4,
+                            {
+                                value: endValue,
+                                roundProps: "value",
+                                ease: Circ.easeOut,
+                                onUpdate: function () {
+                                    document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+                                }
+                            });
+                    })
+                    .addTo(ctrl);
+            });
+        }
+    }
     //nav
     $(".dropdown").hover(
         function () {
@@ -17,33 +197,40 @@ $(document).ready(function () {
             $(this).toggleClass('open');
         }
     );
-    //menu sidebar
-    $(function () {
-        var Accordion = function (el, multiple) {
-            this.el = el || {};
-            this.multiple = multiple || false;
-
-            // Variables privadas
-            var links = this.el.find('.link');
-            // Evento
-            links.on('click', { el: this.el, multiple: this.multiple }, this.dropdown)
+    $(".dropdown").click(
+        function () {
+            $('.dropdown-menu', this).not('.dropdown-menu').stop(true, true).fadeIn("400");
+            $(this).toggleClass('open');
+        },
+        function () {
+            $('.dropdown-menu', this).not(' .dropdown-menu').stop(true, true).fadeOut("400");
+            $(this).toggleClass('open');
         }
-
-        Accordion.prototype.dropdown = function (e) {
-            var $el = e.data.el;
-            $this = $(this),
-                $next = $this.next();
-
-            $next.slideToggle();
-            $this.parent().toggleClass('open');
-
-            if (!e.data.multiple) {
-                $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-            };
+    );
+    
+    //shopping cart
+    $(document).click(function () {
+        var $item = $(".shopping-cart");
+        if ($item.hasClass("active")) {
+            $item.removeClass("active");
         }
-
-        var accordion = new Accordion($('#accordion'), false);
     });
+
+    $('.shopping-cart').each(function () {
+        var delay = $(this).index() * 50 + 'ms';
+        $(this).css({
+            '-webkit-transition-delay': delay,
+            '-moz-transition-delay': delay,
+            '-o-transition-delay': delay,
+            'transition-delay': delay
+        });
+    });
+    $('#cart').click(function (e) {
+        e.stopPropagation();
+        $(".shopping-cart").toggleClass("active");
+    });
+
+
 
 
     $(document).ready(function () {
@@ -54,8 +241,7 @@ $(document).ready(function () {
             if (scrollElement.scrollTop() >= stickyTop) {
                 stickyWrapper.height(stickyHeight);
                 sticky.addClass("is-sticky");
-            }
-            else {
+            } else {
                 sticky.removeClass("is-sticky");
                 stickyWrapper.height('auto');
             }
@@ -163,4 +349,3 @@ $(document).ready(function () {
 
 
 })
-
